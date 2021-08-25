d84cf9bcfe9412cc043d5d518c28cb2da452a688
piaoyizy <925754@qq.com>
2021-08-25 22:35:07 +0800

    .

---
+++ diy-part1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 添加SSR-Plus
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+
+# 添加PassWall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+
+# 添加bypass
+rm -rf  package/luci-app-bypass
+git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+
+
+# Add luci-app-openclash
+git clone -b master https://github.com/vernesong/OpenClash.git package-temp
+rm -rf package/lean/luci-app-openclash
+mv -f package-temp/luci-app-openclash package/lean/
+cat >> .config1 <<EOF
+CONFIG_PACKAGE_luci-app-openclash=y
+CONFIG_LIBCURL_COOKIES=y
+CONFIG_LIBCURL_FILE=y
+CONFIG_LIBCURL_FTP=y
+CONFIG_LIBCURL_HTTP=y
+CONFIG_LIBCURL_NO_SMB="!"
+CONFIG_LIBCURL_OPENSSL=y
+CONFIG_LIBCURL_PROXY=y
+CONFIG_PACKAGE_bash=y
+CONFIG_PACKAGE_ca-bundle=y
+CONFIG_PACKAGE_coreutils-nohup=y
+CONFIG_PACKAGE_curl=y
+CONFIG_PACKAGE_libcurl=y
+CONFIG_PACKAGE_libncurses=y
+CONFIG_PACKAGE_libreadline=y
+CONFIG_PACKAGE_terminfo=y
+EOF
+rm -rf package-temp
---
+++ diy-part2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
+
+#'godproxy'
+# rm -rf package/lean/luci-app-godproxy
+# git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
+
+
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+#'设置默认主题'
+default_theme='edge'	
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
