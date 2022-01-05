d2ce3e95eccf42b00ed3c4f37a2ff9e842195756
piaoyizy <925754@qq.com>
2022-01-06 00:47:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 
 # Add luci-app-unblockneteasemusic
 rm -rf ../lean/luci-app-unblockmusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git /package/unblockneteasemusic
 
 
 # 添加SSR-Plus
