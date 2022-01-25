0bb07c9e41e6a9106e971bd668dc8c8d54741d67
piaoyizy <925754@qq.com>
2022-01-25 16:16:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,9 @@
 
 
 
-git clone  https://github.com/QiuSimons/openwrt-mos.git package/luci-app-mosdns
+# git clone  https://github.com/QiuSimons/openwrt-mos.git package/luci-app-mosdns
 
-sed -i "s/127.0.0.1/127.0.0.1:5335/g"   package/luci-app-mosdns/root/etc/mosdns/set.sh
+# sed -i "s/127.0.0.1/127.0.0.1:5335/g"   package/luci-app-mosdns/root/etc/mosdns/set.sh
 
 # 添加SSR-Plus
 git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
