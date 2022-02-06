c5b2e6855376456e2338e50edb4ce180193d540d
piaoyizy <925754@qq.com>
2022-02-06 16:22:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,6 +22,9 @@ git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lea
 
 git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
 
+sed -i 's/shadowsocksr/passwall/g' package/lean/luci-theme-neobird/luasrc/view/themes/neobird/header.htm
+ 
+ 
 #'设置默认主题'
 default_theme='neobird'	
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
