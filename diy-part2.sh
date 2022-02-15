13289b2f07e0a7b65449a32a999bcd3a34e6802d
piaoyizy <925754@qq.com>
2022-02-15 16:58:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,14 @@ sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
 
 
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
-
 git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
-
 sed -i 's/shadowsocksr/passwall/g' package/lean/luci-theme-neobird/luasrc/view/themes/neobird/header.htm
  
  
+ 
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+
+
 #'设置默认主题'
 default_theme='edge'	
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
