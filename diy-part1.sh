f2641c9460572526a1d08417812d3e6b6c147f96
piaoyizy <925754@qq.com>
2024-03-14 17:00:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,10 +27,7 @@ git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lea
 # 添加科学
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
 
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
