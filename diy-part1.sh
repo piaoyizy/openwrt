ab011abde1c09528f1eef831b7aa80e79cf3dc53
piaoyizy <925754@qq.com>
2024-10-17 22:33:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+git clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
 
 # 添加科学
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
