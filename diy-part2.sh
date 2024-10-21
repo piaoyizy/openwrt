d082871619f404d22a4ed5f3f6aa62a08f1eb35b
piaoyizy <925754@qq.com>
2024-10-21 18:12:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,5 +26,5 @@ sed -i 's/list listen_https/# list listen_https/' package/network/services/uhttp
 
 
 #'设置默认主题'
-default_theme='Edge'	
+default_theme='Argon'	
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
