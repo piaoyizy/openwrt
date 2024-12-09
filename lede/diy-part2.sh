96b56cd92569d9e1f9c1df59af519d60cd21766f
piaoyizy <925754@qq.com>
2024-12-09 16:14:38 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -14,9 +14,9 @@
 sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/luci2/bin/config_generate
 
 
-rm -rf feeds/luci/luci-app-smartdns
+
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
