e1627d41bc80a7cf6877e960c0529d8beecfff32
piaoyizy <925754@qq.com>
2024-12-10 18:33:09 +0800

    ✅

---
+++ lede/diy-part2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/luci2/bin/config_generate
+
+
+rm -rf feeds/luci/luci-app-smartdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+
+sed -i 's/list listen_https/# list listen_https/' package/network/services/uhttpd/files/uhttpd.config
+
+
+#'设置默认主题'
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' ./feeds/luci/collections/luci-light/Makefile
