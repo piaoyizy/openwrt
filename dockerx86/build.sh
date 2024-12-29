09338ab3a0fa53cad3dc77297e353b6ad257ce1d
piaoyizy <925754@qq.com>
2024-12-29 15:24:37 +0800

    Update build.sh

--- dockerx86/build.sh
+++ dockerx86/build.sh
@@ -28,6 +28,5 @@ rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
 (cd "$TMPDIR" && tar cf ../openwrt-x86-64-default-rootfs-patched.tar .) && \
 rm -f DockerImg-OpenwrtArm64-${TAG}.gz && \
 docker build -t ${IMG_NAME}:${TAG} . && \
-rm -f  openwrt-x86-64-default-rootfs-patched.tar && \
 rm -rf "$TMPDIR" && \
 docker save ${IMG_NAME}:${TAG} | pigz -9 > docker-img-openwrt-x86-${TAG}.gz
